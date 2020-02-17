62880b4d38b53cccfb0947aea31c2c24a139550d
antsanti7 <60456844+antsanti7@users.noreply.github.com>
2020-02-16 23:52:08 -0500

    Follow Cam

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -53,17 +53,31 @@ public class Slingshot : MonoBehaviour
         if (!aimingMode)
             return;                                                   // b
         // Get the current mouse position in 2D screen coordinates
-        Vector3mousePos2D = Input.mousePosition;                                  // c
-        mousePos2D.z = -Camera.main.transform.position.z; Vector3mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        Vector3 mousePos2D = Input.mousePosition;                                  // c
+        mousePos2D.z = -Camera.main.transform.position.z; 
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
         // Find the delta from the launchPos to the mousePos3D
-        Vector3mouseDelta = mousePos3D - launchPos;
+        Vector3 mouseDelta = mousePos3D - launchPos;
         // Limit mouseDelta to the radius of the SlingshotSphereCollider         // d
-        floatmaxMagnitude = this.GetComponent<SphereCollider>().radius;
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
 
         if (mouseDelta.magnitude > maxMagnitude)
         {
-            mouseDelta.Normalize(); mouseDelta *= maxMagnitude;
+            mouseDelta.Normalize(); 
+            mouseDelta *= maxMagnitude;
         }
 
+        Vector3 projPos = launchPos + mouseDelta; 
+        projectile.transform.position = projPos; 
+        
+        if (Input.GetMouseButtonUp(0))
+        {                                        // e
+            // The mouse has been released
+            aimingMode = false;
+            projectileRigidbody.isKinematic = false;
+            projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
+            projectile = null;
+        }
     }
 }
