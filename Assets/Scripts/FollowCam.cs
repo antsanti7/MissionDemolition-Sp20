859a1e0106a710271efd1b27629e17b8d57b3c20
antsanti7 <60456844+antsanti7@users.noreply.github.com>
2020-02-17 01:51:19 -0500

    Projectile Tracer

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -17,13 +17,33 @@ public class FollowCam : MonoBehaviour
     {
         camZ = this.transform.position.z;
     } 
+
     void FixedUpdate()
     {
-    // if there's only one line following an if, it doesn't needbraces
-        if(POI ==null)
-            return; 
-                  
-    Vector3 destination = POI.transform.position;
+ 
+    Vector3 destination;
+        // If there is no poi, return to P:[ 0, 0, 0 ]
+        if(POI ==null) 
+        {
+            destination =Vector3.zero;
+        }
+        else
+        {
+            // Get the position of the poi
+            destination = POI.transform.position;
+            // If poi is a Projectile, check to see if it's at rest
+            if(POI.tag =="Projectile") 
+            {
+                // if it is sleeping (that is, not moving)
+                if (POI.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    // return to default view
+                    POI =null;
+                    // in the next updatereturn;
+                }
+            }
+        }
+
     destination.x = Mathf.Max(minXY.x, destination.x); 
     destination.y = Mathf.Max(minXY.y, destination.y);
     destination = Vector3.Lerp(transform.position, destination, easing);
